5093709a27e07a6b423b33d267c4baa16bef466a
Can I be discharged? <kmiiiaa@pusan.ac.kr>
2020-06-23 18:32:26 +0900

    Add interrupter and some declaration
    Add interrupter
    Add some function declaration which use Serial

--- LED/LedPlayer.cpp
+++ LED/LedPlayer.cpp
@@ -20,12 +20,16 @@ void LedPlayer::init()
             digitalWrite(pin::ledcolorpins[i][j], LOW);
         }
     }
-
-    myPattern[0].pattern = pattern2;  // 패턴 지정
+    initTimer1();
+    send_max_pattern();
+    myPattern[0].pattern = pattern1;  // 패턴 지정
     myPattern[0].init(150, 150, 150); //색상 지정
+    myPattern[1].pattern = pattern2;  // 패턴 지정
+    myPattern[1].init(150, 150, 150); //색상 지정
+    curr_pattern = 1; //테스트 코드, 출력하고 싶은 패턴으로
 }
 
-void LedPlayer::operator()(const uint8_t button)
+void LedPlayer::operator()(uint8_t& button)
 {
     /*
         pattern[button]()을 호출
@@ -45,7 +49,56 @@ void LedPlayer::run(void)
     }
 }
 
-void pattern1(uint8_t button, uint8_t *color)
+ISR(TIMER1_OVF_vect) //Timer1 Service
+{
+    /*
+        if serail available -> read serial and renew player.button
+        if button == 3 -> player.curr_pattern+=1; player.curr_pattern%=player.max_pattern;
+    */
+
+}
+
+
+#define RESOLUTION 65536        // Timer1 is 16 bit
+void LedPlayer::initTimer1(void) //initialize Timer1 to 100us overflow
+{
+    TCCR1A = 0;          // clear control register A
+    TCCR1B = _BV(WGM13); // set mode as phase and frequency correct pwm, stop the timer
+
+    long cycles;
+    long microseconds = 500; //setup microseconds here
+    unsigned char clockSelectBits;
+    cycles = (F_CPU / 2000000) * microseconds; // the counter runs backwards after TOP, interrupt is at BOTTOM so divide microseconds by 2
+    if (cycles < RESOLUTION)
+        clockSelectBits = _BV(CS10); // no prescale, full xtal
+    else if ((cycles >>= 3) < RESOLUTION)
+        clockSelectBits = _BV(CS11); // prescale by /8
+    else if ((cycles >>= 3) < RESOLUTION)
+        clockSelectBits = _BV(CS11) | _BV(CS10); // prescale by /64
+    else if ((cycles >>= 2) < RESOLUTION)
+        clockSelectBits = _BV(CS12); // prescale by /256
+    else if ((cycles >>= 2) < RESOLUTION)
+        clockSelectBits = _BV(CS12) | _BV(CS10); // prescale by /1024
+    else
+        cycles = RESOLUTION - 1, clockSelectBits = _BV(CS12) | _BV(CS10); // request was out of bounds, set as maximum
+
+    ICR1 = cycles;
+    TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));
+    TCCR1B |= clockSelectBits; // reset clock select register, and starts the clock
+
+    TIMSK1 = _BV(TOIE1);
+    TCNT1 = 0;
+    sei(); //enable global interrupt
+}
+
+
+/*
+    button 사용 후에는 99로 초기화
+    button이 99면 패턴 호출 x
+    패턴이 진행중일때에도 button이 계속 99인지 체크하고 아니면 바로 종료
+*/
+unsigned long long pre_time,curr_time;
+void pattern1(uint8_t& button, uint8_t *color)
 {
     /*
         첫번째 패턴 : 십자가
@@ -53,13 +106,15 @@ void pattern1(uint8_t button, uint8_t *color)
     uint8_t row = button / pin::num_led_rows;
     uint8_t col = button % pin::num_led_rows;
 
-    for (int j = 0; j < 4; j++)
+    button = 99;//initialize
+
+    for (int j = 0; j < 4&&button==99; j++)
     {
         if (j == 0)
         {
             pre_time = millis();
             curr_time = millis();
-            while (curr_time - pre_time < 100)
+            while (curr_time - pre_time < 100 && button == 99)
             {
               curr_time = millis();
               digitalWrite(pin::ledselpins[col + j], LOW);
@@ -72,7 +127,7 @@ void pattern1(uint8_t button, uint8_t *color)
         {
             pre_time = millis();
             curr_time = millis();
-            while (curr_time - pre_time < 100)
+            while (curr_time - pre_time < 100&& button==99)
             {
                 curr_time = millis();
                 if (col + j < pin::num_led_columns && col + j >= 0)
@@ -103,21 +158,23 @@ void pattern1(uint8_t button, uint8_t *color)
     }
 }
 
-void pattern2(uint8_t button, uint8_t *color)
+void pattern2(uint8_t& button, uint8_t *color)
 {
     /*
-        첫번째 패턴 : 엑스
+        두번째 패턴 : 엑스
     */
     uint8_t row = button / pin::num_led_rows;
     uint8_t col = button % pin::num_led_rows;
 
-    for (int j = 0; j < 4; j++)
+    button = 99; //initialize
+
+    for (int j = 0; j < 4&&button == 99; j++)
     {
         if (j == 0)
         {
             pre_time = millis();
             curr_time = millis();
-            while (curr_time - pre_time < 100)
+            while (curr_time - pre_time < 100 && button == 99)
             {
               curr_time = millis();
               digitalWrite(pin::ledselpins[col + j], LOW);
@@ -130,7 +187,7 @@ void pattern2(uint8_t button, uint8_t *color)
         {
             pre_time = millis();
             curr_time = millis();
-            while (curr_time - pre_time < 100)
+            while (curr_time - pre_time < 100 && button == 99)
             {
                 curr_time = millis();
                 if (col + j < pin::num_led_columns && col + j >= 0)
--- LED/LedPlayer.h
+++ LED/LedPlayer.h
@@ -4,20 +4,25 @@
 #include "pattern.h"
 #include <SoftwareSerial.h>
 
-class LedPlayer{
-    static const uint8_t max_pattern = 1;//패턴의 경우의 수 저장.
+class LedPlayer
+{
+public:
+    static const uint8_t max_pattern = 2; //패턴의 경우의 수 저장.
+    uint8_t button = 99;                  //사용 후 반드시 99로 초기화해주기
+    void init();                          //uart 통신을 통해서 max_pattern을 myPlayer로 넘겨줌. //setup에서 호출
+    void run(void);                       //loop안에서 돌아가는 함수
+private:
     pattern_ myPattern[max_pattern];
     uint8_t curr_pattern = 0;
-    void operator()(const uint8_t);//인자로 들어오는 패턴을 호출;
-public:
-    void init();//uart 통신을 통해서 max_pattern을 myPlayer로 넘겨줌. //setup에서 호출
-    void run(void); //loop안에서 돌아가는 함수
-    ~LedPlayer(){}
+    void operator()(uint8_t&); //인자로 들어오는 패턴을 호출;
+    void initTimer1(void);
+    void send_max_pattern(void){}//시작시 시리얼로 max_pattern 전송해줌.
 };
 
-extern void pattern1(uint8_t, uint8_t*);
-extern void pattern2(uint8_t, uint8_t*);
-extern LedPlayer player;
 extern unsigned long long pre_time, curr_time;
+extern void pattern1(uint8_t&, uint8_t *);
+extern void pattern2(uint8_t&, uint8_t *);
+extern LedPlayer player;
+
 
 #endif
